812505c09e596910024477f3ad0adb761051641d
ansungwoo <asw4ever@naver.com>
2024-04-04 23:11:08 +0900

    feat: 페이지 별로 인덱싱 해주는 기능 추가 게시글 목록 하단에 다음 페이지 이전페이지 기능

---
+++ backend/ggok/ggok/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from ggok.models import Question, Answer
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question  # 사용할 모델
+        fields = ['subject', 'content']  # QuestionForm에서 사용할 Question 모델의 속성
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
--- backend/ggok/ggok/urls.py
+++ backend/ggok/ggok/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
+
 app_name = 'ggok'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
