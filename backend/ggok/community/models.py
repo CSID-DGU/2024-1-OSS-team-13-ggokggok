14a54f7d24ac70f9d323eb77c748c18cf6dae2bf
ansungwoo <asw4ever@naver.com>
2024-04-09 17:15:34 +0900

    docs: community앱 게시글, 답변 기능 api 수정 model 이름 수정, post, commnet 수정 기능 추가

---
+++ backend/ggok/community/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_author') # 계정이 삭제되면 이 계정이 작성한 질문을 삭제하도록
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    post_region = models.CharField(max_length=30)
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='post_voter')  # 추천인 추가
+    def __str__(self):
+        return self.subject
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answer_author')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='answer_voter')
+
+    def __str__(self):
+        return f"Answer to {self.post.subject} by {self.author.username}"
+    #장고 관리자에서 보기 편하도록 만들어둔 것
