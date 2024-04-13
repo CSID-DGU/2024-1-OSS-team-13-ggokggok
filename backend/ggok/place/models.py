2d5ad589de62ca78e7b6b4fa0915d7c67387f3c7
soyoon <weehee6947@gmail,com>
2024-04-13 16:47:38 +0900

    feat : 명소 커뮤니티 게시글 + 댓글

--- backend/ggok/place/models.py
+++ backend/ggok/place/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PlacePost(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question') # 계정이 삭제되면 이 계정이 작성한 질문을 삭제하도록
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    place_id = models.CharField(max_length=30)
+    #modify_date = models.DateTimeField(null=True, blank=True) 수정 일시는 일단 기능 추가 안함.
+    recommend = models.ManyToManyField(User, related_name='voter_question')  # 추천인 추가
+    public = models.BooleanField()
+    review = models.IntegerField()
+    category = models.CharField(max_length=50)
+    def __str__(self):
+        return self.title
+
+
+class PlaceComment(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
+    placepost = models.ForeignKey(PlacePost, on_delete=models.CASCADE)
+    content = models.TextField()
+    recommend = models.ManyToManyField(User, related_name='voter_question')
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Answer to {self.post.title} by {self.author.username}"
