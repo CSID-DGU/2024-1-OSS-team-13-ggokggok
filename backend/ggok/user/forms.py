a8ff1bb7ab3272c29f50852c25a65b9a7c483c47
ansungwoo <asw4ever@naver.com>
2024-04-14 17:18:57 +0900

    docs: 회원가입 view파일 수정, model 이름 수정

--- backend/ggok/user/forms.py
+++ backend/ggok/user/forms.py
@@ -1,12 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from user.models import user_info
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
+    region1 = forms.CharField(label="지역1", required=False)  # 지역1 필드 추가
+    region2 = forms.CharField(label="지역2", required=False)  # 지역2 필드 추가
+    name = forms.CharField(label="이름")  # 이름 필드 추가
 
     class Meta:
         model = User
-        fields = ("username", "password1", "password2", "email")
+        fields = ("username", "password1", "password2", "email", "region1", "region2", "name")  # 지역1, 지역2, 이름 필드 추가
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop('password1')  # 비밀번호 필드 감추기
+        self.fields.pop('password2')  # 비밀번호 확인 필드 감추기
--- backend/ggok/user/views.py
+++ backend/ggok/user/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from user.forms import UserForm
+
+@api_view(['POST'])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return Response({'detail': '로그인 성공'})
+    else:
+        return Response({'detail': '유효하지 않은 인증 정보'}, status=400)
+
+@api_view(['POST'])
+def logout_view(request):
+    logout(request)
+    return Response({'detail': '로그아웃 성공'})
+
+@api_view(['POST'])
+def signup_view(request):
+    if request.method == "POST":
+        form = UserForm(request.data)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return Response({'detail': '회원가입 및 로그인 성공'})
+    return Response(form.errors, status=400)
