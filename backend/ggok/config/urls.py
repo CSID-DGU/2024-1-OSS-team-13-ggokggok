333e2ac90060cf0c19de37b401c1a3bb462e3711
ansungwoo <asw4ever@naver.com>
2024-04-14 17:03:26 +0900

    chore: DB sqlite3에서 postgresql로 변경.

--- backend/ggok/config/urls.py
+++ backend/ggok/config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-import community.api
-from community.api import PostViewSet, CommentViewSet
+import community.serializers
+from community.serializers import PostViewSet, CommentViewSet
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import routers
 from rest_framework.permissions import AllowAny
+from community.views.base_views import index
 
 app_name='community'
 
@@ -32,4 +33,5 @@ urlpatterns = [
     path('api/', include(post_router.urls)),
     path('api/', include(comment_router.urls)),  # community/answer/에 대한 URL 변경
     path('api/doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', index, name='index'),
 ]
