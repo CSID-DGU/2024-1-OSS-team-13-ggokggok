df00c6bab6ba2ae0df683e07897690915d69bc39
soyxni <weehee6947@gmail.com>
2024-04-15 15:14:40 +0900

    feat : 명소 정보 model 추가

--- backend/ggok/place/models.py
+++ backend/ggok/place/models.py
@@ -28,11 +28,3 @@ class PlaceComment(models.Model):
     def __str__(self):
         return f"Answer to {self.post.title} by {self.author.username}"
 
-class PlaceInfo(models.Model):
-    id = models.AutoField(primary_key=True) #등록로직변경필요
-    name = models.CharField(max_length=200) #상호명
-    address = models.TextField() #주소_텍스트
-    lat = models.FloatField
-    review = models.IntegerField() #추천 판단용 지표값
-    category = models.CharField(max_length=50)
-    written = models.ManyToManyField(User, related_name='written_place')  # 글 쓴 사람수 ..사용처는 미정
--- backend/ggok/placesinfo/models.py
+++ backend/ggok/placesinfo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PlaceInfo(models.Model):
+    id = models.AutoField(primary_key=True) #등록로직변경필요
+    name = models.CharField(max_length=200) #상호명
+    address = models.TextField() #주소_텍스트
+    lat = models.FloatField
+    review = models.IntegerField() #추천 판단용 지표값
+    category = models.CharField(max_length=50)
+    written = models.ManyToManyField(User, related_name='written_place')  # 글 쓴 사람수 ..사용처는 미정
