a769f4e9d447db2943e73950fcda1747cdbff79a
soyxni <weehee6947@gmail.com>
2024-04-15 18:24:18 +0900

    refactor : place 앱 모델 수정

--- backend/ggok/place/views/post_views.py
+++ backend/ggok/place/views/post_views.py
@@ -15,15 +15,15 @@ def post_create(request):
     if request.method == 'POST':
 
         #명소 위도+경도 받아와서 확인
-        place_lat = request.data.get('place_lat')
-        place_long = request.data.get('place_long')
+        place_lat = request.data.get('lat')
+        place_long = request.data.get('long')
         isPlace = PlaceInfo.objects.filter(lat=place_lat, long=place_long)
 
         if isPlace:
             place_id = isPlace.id
         else: #없으면 새로 등록
-            place_name = request.data.get('place_name')
-            place_address = request.data.get('place_address')
+            place_name = request.data.get('name')
+            place_address = request.data.get('address')
             newPlace = PlaceInfo.objects.create(name=place_name, address=place_address, lat=place_lat, long=place_long)
             place_id = newPlace.id
 
