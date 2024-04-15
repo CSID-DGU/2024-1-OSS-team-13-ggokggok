c8fd941aa801ea2c307769f7bf8fadcc7c111fd4
ansungwoo <132185864+aeeengsungwoo@users.noreply.github.com>
2024-04-15 18:49:52 +0900

    postgres -> root

--- backend/ggok/config/settings.py
+++ backend/ggok/config/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ggok',
-        'USER': 'postgres',
+        'USER': 'root',
         'PASSWORD': '0000',
         'HOST': 'localhost',
         'PORT': '',
