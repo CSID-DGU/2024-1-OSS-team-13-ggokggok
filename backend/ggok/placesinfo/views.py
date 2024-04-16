683c5f60595e4e9c3803f910207a029b9d75545d
soyxni <weehee6947@gmail.com>
2024-04-16 16:18:32 +0900

    feat : 명소 선정 기능 추가(별점 계산)

--- backend/ggok/placesinfo/views.py
+++ backend/ggok/placesinfo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import PlaceInfo
+from ..place.models import PlacePost
 
-# Create your views here.
+def update_review(place_id):
+    posts = PlacePost.objects.filter(place_id=place_id)
+    total = sum([post.reviw for post in posts])
+    average = total / len(posts) if len(posts) > 0 else 0
+    place_info = PlaceInfo.objects.get(id=place_id)
+    place_info.review = average
+    place_info.save()
+
