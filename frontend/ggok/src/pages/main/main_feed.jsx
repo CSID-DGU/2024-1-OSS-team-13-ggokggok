479994fa1de27bee850c3f41898d2c0d1e70e35d
jeong <jeong235711@gmail.com>
2024-04-17 14:24:01 +0900

    fixed : 메인커뮤니티페이지수정/ 명소구역 백과 연결 및 화면 구현

--- frontend/ggok/src/pages/main/main_feed.jsx
+++ frontend/ggok/src/pages/main/main_feed.jsx
@@ -17,7 +17,7 @@ const SubTitle = styled.h2`
 
 const ContentBox = styled.div`
   height: 143px;
-  width: 95%;
+  width: 90%;
   border: 1px solid #C9B6A9;
   border-radius: 10px;
   margin: 15px 0 0;
@@ -81,7 +81,18 @@ export default function main_feed(){
         }
     }
 
-    useEffect(() =>{fetchData();}, []);
+    const [getplace, setplace] = useState([]);
+
+    async function fetchPlace() {
+        try {
+          const response = await axios.get('http://localhost:8000/place/post/');
+          setplace(response.data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+    }
+
+    useEffect(() =>{fetchData(); fetchPlace();}, []);
 
     console.log(getData);
 
@@ -94,9 +105,20 @@ export default function main_feed(){
           </Title>
             <SubTitle>
             <h2>우리지역 HOT 명소</h2>
-            <ContentBox>
-                
-            </ContentBox>
+            <div style= {{ overflow: 'auto', height: '200px' }}>
+                {getplace.length > 0 ? (
+                        getplace.map((data) => (
+                            <ContentBox>
+                            <div style={{display: 'flex'}}>
+                                <ContentImg src="/"></ContentImg>
+                                <div>
+                                    <h3>{data.title}</h3>
+                                    <p>{data.content}</p>
+                                </div>
+                            </div>
+                            </ContentBox>
+                ))): (<></>)}
+                </div>
             </SubTitle>
 
             
