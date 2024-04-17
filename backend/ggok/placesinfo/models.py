5320f0a61a82327500841d5d1dfb8019b69f9af0
ansungwoo <asw4ever@naver.com>
2024-04-17 18:28:54 +0900

    fix: placeinfo, place 버그 수정

--- backend/ggok/placesinfo/models.py
+++ backend/ggok/placesinfo/models.py
@@ -10,3 +10,4 @@ class PlaceInfo(models.Model):
     review = models.IntegerField() #추천 판단용 지표값
     category = models.CharField(max_length=50)
     written = models.ManyToManyField(UserInfo, related_name='written_place')  # 글 쓴 사람수 ..사용처는 미정
+    objects = models.Manager()
--- backend/ggok/placesinfo/views.py
+++ backend/ggok/placesinfo/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-from .models import PlaceInfo
-from ..place.models import PlacePost
 
+from .models import PlaceInfo
 def update_review(place_id):
+    from place.models import PlacePost  # 함수 내에서 import
     posts = PlacePost.objects.filter(place_id=place_id)
-    total = sum([post.reviw for post in posts])
+    total = sum([post.review for post in posts])  # 'review' 오타 수정
     average = total / len(posts) if len(posts) > 0 else 0
     place_info = PlaceInfo.objects.get(id=place_id)
     place_info.review = average
     place_info.save()
+
 
