852685061905980f6d06018e165580bc348c28bd
soyxni <weehee6947@gmail.com>
2024-05-21 17:26:33 +0900

    feat : 마이페이지 유저 글 불러오기

--- backend/ggok/user/serializers.py
+++ backend/ggok/user/serializers.py
@@ -1,5 +1,8 @@
 from user.models import UserInfo
 from rest_framework import serializers
+from community.models import Post
+from place.models import PlacePost
+
 
 class JoinSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,3 +11,22 @@ class JoinSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=255)
     password = serializers.CharField(max_length=128, write_only=True)
+
+class UserPostQuerySerializer(serializers.Serializer):
+    post = serializers.CharField(required=True)
+    author = serializers.CharField(required=True)
+
+    def validate(self, data):
+        if not data.get('post') or not data.get('author'):
+            raise serializers.ValidationError("Both 'post' and 'userID' must be provided.")
+        return data
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class PlacePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlacePost
+        fields = '__all__'
--- backend/ggok/user/urls.py
+++ backend/ggok/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from user.views import LoginView, LogoutView, JoinView
+from user.views import LoginView, LogoutView, JoinView, UserPostSearch
 app_name = 'user'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('join/', JoinView.as_view(), name='join'),
+    path('user_post_search/', UserPostSearch, name='user_post_search'),
 ]
