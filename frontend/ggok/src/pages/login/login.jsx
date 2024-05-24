df08da4289ca57bc396fd4ba712d5f49957f8526
enunsnv <seoej0315@naver.com>
2024-05-25 01:06:32 +0900

    feat: 모든 지역 null인 경우 지역 등록 페이지로 이동

--- frontend/ggok/src/pages/login/login.jsx
+++ frontend/ggok/src/pages/login/login.jsx
@@ -5,7 +5,6 @@ import logo from "../..//others/img/logo-icon.png";
 import { Title, Wrapper, Blank, TitleDiv, LogoImage } from "../../styles/Styles";
 import axios from "axios";
 
-
 const Form = styled.form`
   align-items: center;
   margin-top: 30px;
@@ -38,37 +37,26 @@ const Input = styled.input`
   }
 `;
 
-
 const Error = styled.span`
   font-weight: 600;
   color: tomato;
 `;
 
-const Switcher = styled.span`
-  margin-top: 20px;
-  a {
-    color: #1d9bf0;
-  }
-`;
-
 export default function Login() {
   const nav = useNavigate();
   const [isLoading, setLoading] = useState(false);
-
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
 
   const onSubmit = async (e) => {
     e.preventDefault();
     setError("");
 
-    if (id === "" || password === "" || isLoading) return;
+    if (email === "" || password === "" || isLoading) return;
 
     const postData = {
-      username: id,
+      username: email,
       password: password,
     };
 
@@ -78,12 +66,17 @@ export default function Login() {
         "https://port-0-ggokggok-1cupyg2klvrp1r60.sel5.cloudtype.app/user/login/",
         postData
       );
-      console.log("data");
+      console.log("data", response.data);
       if (response.data.success) {
-        sessionStorage.clear();
         sessionStorage.setItem("user", JSON.stringify(response.data));
-        nav("/");
-      } else {
+        const userData = JSON.parse(sessionStorage.getItem('user'));
+        if (userData.data.region1 === null && userData.data.region2 === null) {
+          nav("/set-region"); 
+        } else {
+          nav("/");
+        }
+      }
+ else {
         setError("로그인 실패. 아이디와 비밀번호를 확인하세요.");
       }
     } catch (error) {
@@ -95,29 +88,23 @@ export default function Login() {
   };
 
   const onChange = (e) => {
-    const {
-      target: { name, value },
-    } = e;
-  
-    if (name === "email") { // name이 "email"인 경우에만 setEmail 호출
+    const { name, value } = e.target;
+    if (name === "email") {
       setEmail(value);
     } else if (name === "password") {
       setPassword(value);
     }
   };
-  
 
   return (
     <Wrapper>
-      
       <Title>
         <Blank/><Blank/><Blank/>
-          <TitleDiv>
+        <TitleDiv>
           <LogoImage src={logo} alt="Logo" />
-            <span>로그인</span>
-          </TitleDiv>
+          <span>로그인</span>
+        </TitleDiv>
       </Title>
-
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
@@ -135,12 +122,9 @@ export default function Login() {
           type="password"
           required
         />
-        
-        <Link to = "/create-account" style={{ textDecoration: "none"}}><div color="A3CCAA">회원이 아니신가요?</div></Link>
         <Input style={{backgroundColor: "#A3CCAA"}} type="submit" value={isLoading ? "Loading..." : "로그인"} />
-        
       </Form>
-      {error !== "" ? <Error>{error}</Error> : null}
+      {error !== "" && <Error>{error}</Error>}
     </Wrapper>
   );
 }
