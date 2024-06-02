0b3b126e8863314b27088cde8bcc661c76edf7b4
ansungwoo <asw4ever@naver.com>
2024-06-02 16:53:27 +0900

    fix: user signals region1을 삭제할 경우에, region2가 있는 경우에만 실행되도록 수정

--- backend/ggok/user/signals.py
+++ backend/ggok/user/signals.py
@@ -5,7 +5,7 @@ from user.models import UserInfo
 @receiver(post_save, sender=UserInfo)
 def switch_region(sender, instance, **kwargs):
     # instance.region1이 빈 문자열일 경우에만 실행
-    if instance.region1 == "":
+    if instance.region1 == "" and instance.region2 != "":
         instance.region1 = instance.region2
         instance.region2 = ""
         instance.save(update_fields=["region1", "region2"])
