eeefedcaa9fba6b81dfe4702c1b8cedf5e654a22
enunsnv <seoej0315@naver.com>
2024-06-03 18:26:02 +0900

    fix: font 형식 수정

--- frontend/ggok/src/styles/GlobalStyle.jsx
+++ frontend/ggok/src/styles/GlobalStyle.jsx
@@ -6,13 +6,13 @@ import reset from "styled-reset";
 const GlobalStyle = createGlobalStyle`
     ${reset}
     @font-face {
-    font-family: "basicTTF";
+    font-family: "basicOTF";
       font-style: normal;
       src: url("src/others/font/laundryRegular.otf") format("opentype")
     }
 
     @font-face {
-      font-family: "basicOTF";
+      font-family: "basicTTF";
       font-style: normal;
       src: url("src/others/font/laundryRegular.ttf") format("truetype");
   }
