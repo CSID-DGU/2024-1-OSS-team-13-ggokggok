97654c435f6bd2e8615d8161c5a37716c7cc2507
enunsnv <seoej0315@naver.com>
2024-06-03 18:36:53 +0900

    fix: font 형식 수정

--- frontend/ggok/src/styles/GlobalStyle.jsx
+++ frontend/ggok/src/styles/GlobalStyle.jsx
@@ -6,19 +6,18 @@ import reset from "styled-reset";
 const GlobalStyle = createGlobalStyle`
     ${reset}
     @font-face {
-    font-family: "basicOTF";
-      font-style: normal;
-      src: url("src/others/font/laundryRegular.otf") format("opentype")
+    font-family: "basicWOfF";
+      src: url("/src/others/font/laundryRegular.woff") format("woff")
     }
 
     @font-face {
       font-family: "basicTTF";
       font-style: normal;
-      src: url("src/others/font/laundryRegular.ttf") format("truetype");
+      src: url("/src/others/font/laundryRegular.ttf") format("truetype");
   }
 
     body {
-        font-family: "basicOTF";
+        font-family: "basicWOFF";
     }
 `;
 
